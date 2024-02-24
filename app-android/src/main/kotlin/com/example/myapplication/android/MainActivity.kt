efec61a44f4d038de995e7fa1c6ea7a882fa7a61
boringx <perkeboring@gmail.com>
2024-02-24 15:50:40 +0300

    delete unused feature (not requirement from test tasks)

--- app-android/src/main/kotlin/com/example/myapplication/android/MainActivity.kt
+++ app-android/src/main/kotlin/com/example/myapplication/android/MainActivity.kt
@@ -37,40 +37,4 @@ class MainActivity : ComponentActivity() {
         window.navigationBarColor = color.toArgb()
         window.statusBarColor = color.toArgb()
     }
-
-    override fun onResume() {
-        super.onResume()
-        val decor = window?.decorView ?: return
-        val uiMode = resources.configuration.uiMode
-        val systemIsDark =
-            (uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
-
-//        changeStatusBarContrastStyle(decor, isStatusBarIconLight(systemIsDark))
-    }
-
-    // TODO: ???
-    private fun isStatusBarIconLight(systemIsDark: Boolean) = false
-
-    @Suppress("Deprecated")
-    private fun changeStatusBarContrastStyle(decor: View, isLightIcons: Boolean) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val windowInsetsController = decor.windowInsetsController ?: return
-            windowInsetsController.setSystemBarsAppearance(
-                if (isLightIcons) {
-                    0
-                } else {
-                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-                },
-                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-            )
-        } else {
-            if (isLightIcons) {
-                decor.systemUiVisibility =
-                    decor.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-            } else {
-                decor.systemUiVisibility =
-                    decor.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            }
-        }
-    }
 }
