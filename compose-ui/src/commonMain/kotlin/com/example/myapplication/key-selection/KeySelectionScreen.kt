89de46057b806b07ad83b38863a3b3c31f08a41f
boringx <perkeboring@gmail.com>
2024-02-24 16:14:12 +0300

    delete unused itemsIndexed

--- compose-ui/src/commonMain/kotlin/com/example/myapplication/key-selection/KeySelectionScreen.kt
+++ compose-ui/src/commonMain/kotlin/com/example/myapplication/key-selection/KeySelectionScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +18,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.RawEntry
-import com.example.myapplication.shared.`bottom-bar`.BottomBarTabEnum
 import com.example.myapplication.Title
 
 @Composable
@@ -55,7 +54,7 @@ private fun Data(
 ) {
     val rememberLockers = remember { lockers.toList() }
     LazyColumn {
-        itemsIndexed(rememberLockers) { i, it ->
+        items(rememberLockers) {
             Divider(
                 color = Color.Black,
                 thickness = 1.dp
